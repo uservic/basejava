fa10d3cbc84d73d110aa9c9f4d9cb585cebbf926
Victor <vic1.wmail@gmail.com>
2018-06-24 16:39:13 +0400

    small changes

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,18 +8,18 @@ public class ArrayStorage {
     private int size = 0;
 
     void clear() {
-        Arrays.fill(storage, 0, size(), null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     void save(Resume r) {
-        int indexForNewElement = size();
+        int indexForNewElement = size;
         storage[indexForNewElement] = r;
         size++;
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
@@ -28,13 +28,10 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        if (size < 1) {
-            return;
-        }
-        for (int i = 0; i < size(); i++) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size() - 1];
-                storage[size() - 1] = null;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 size--;
                 break;
             }
@@ -45,7 +42,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size());
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {
