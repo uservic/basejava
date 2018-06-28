ad96225cbd4668ae24190f687255619aa02937ba
Victor <vic1.wmail@gmail.com>
2018-06-28 18:41:31 +0400

    ++ inteface Storage

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -20,9 +20,7 @@ public class Resume {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         Resume resume = (Resume) o;
-
         return uuid.equals(resume.uuid);
     }
 
