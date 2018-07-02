d9c8771da6c9efe5714ae3c3d0a062ddaa7d08eb
Victor <vic1.wmail@gmail.com>
2018-07-02 23:06:41 +0400

    impl methods in SortedArrayStorage

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -5,31 +5,57 @@ import ru.javawebinar.basejava.model.Resume;
 import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-    private Resume[] sortedStorage = getSortedStorage();
 
     @Override
     public void clear() {
-
+        Arrays.fill(storage,0, size,null);
+        size = 0;
     }
 
     @Override
     public void update(Resume resume) {
-
+        int index = getIndex(resume.getUuid());
+        if (index >= 0) {
+            storage[index] = resume;
+        } else {
+            System.out.println("Resume " + resume.getUuid() + " not found");
+        }
     }
 
     @Override
     public void save(Resume resume) {
-
+        int index = getIndex(resume.getUuid());
+        if (index >= 0) {
+            System.out.println("Resume " + resume.getUuid() + " is already in list");
+        } else if (size == STORAGE_LIMIT) {
+            System.out.println("Storage capacity overflow");
+        } else {
+            int indexForInsert = (index + 1) * (-1);
+            for (int i = size; i >= indexForInsert + 1; i--) {
+                storage[i] = storage[i - 1];
+            }
+            storage[indexForInsert] = resume;
+            size++;
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume with " + uuid + " not found");
+        } else {
+            for (int i = index + 1; i < size; i++) {
+                storage[i - 1] = storage[i];
+            }
+            size--;
+            storage[size] = null;
+        }
     }
 
     @Override
     public Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     @Override
@@ -37,11 +63,5 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, size, searchKey);
-    }
-
-    private Resume[] getSortedStorage() {
-
-
-        return null;
     }
 }
