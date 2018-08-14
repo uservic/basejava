2d7e4f79ebb2138da5690a83a877826b201ae692
Victor <vic1.wmail@gmail.com>
2018-08-14 22:51:34 +0400

    hw11 deadlock impl_2 refactored

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -5,30 +5,21 @@ public class MainDeadLock {
         Object o1 = new Object();
         Object o2 = new Object();
 
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (o1) {
-                    System.out.println("some code1");
-                    synchronized (o2) {
-                        System.out.println("some code2");
-                    }
-                }
-            }
-        });
+        Thread t1 = getThread(o1, o2);
+        Thread t2 = getThread(o2, o1);
 
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (o2) {
-                    System.out.println("some code3");
-                    synchronized (o1) {
-                        System.out.println("some code4");
-                    }
-                }
-            }
-        });
         t1.start();
         t2.start();
+    }
+
+    private static Thread getThread(Object o1, Object o2) {
+       return new Thread(() -> {
+           synchronized (o1) {
+               System.out.println("some code1");
+               synchronized (o2) {
+                   System.out.println("some code2");
+               }
+           }
+       });
     }
 }
