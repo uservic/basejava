0c68be58507a650fa8727cf4b25d223f53e3f3c7
Victor <vic1.wmail@gmail.com>
2018-08-14 22:43:30 +0400

    hw11 deadlock impl_2

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -2,69 +2,33 @@ package ru.javawebinar.basejava;
 
 public class MainDeadLock {
     public static void main(String[] args) {
-        Cat cat = new Cat("Tom", 1, null);
-        Mouse mouse = new Mouse("Jerry", 1, cat);
-        cat.setMouse(mouse);
+        Object o1 = new Object();
+        Object o2 = new Object();
 
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                cat.printCatName();
+                synchronized (o1) {
+                    System.out.println("some code1");
+                    synchronized (o2) {
+                        System.out.println("some code2");
+                    }
+                }
             }
         });
 
         Thread t2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                mouse.printMouseName();
+                synchronized (o2) {
+                    System.out.println("some code3");
+                    synchronized (o1) {
+                        System.out.println("some code4");
+                    }
+                }
             }
         });
         t1.start();
         t2.start();
-    }
-}
-
-class Cat {
-    private String name;
-    private int age;
-    private Mouse mouse;
-
-    public Cat(String name, int age, Mouse mouse) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void setMouse(Mouse mouse) {
-        this.mouse = mouse;
-    }
-
-    public synchronized void printCatName() {
-        System.out.println(name);
-        mouse.printMouseAge();
-    }
-
-    public synchronized void printCatAge() {
-        System.out.println(age);
-    }
-}
-
-class Mouse {
-    private String name;
-    private int age;
-    private Cat cat;
-
-    public Mouse(String name, int age, Cat cat) {
-        this.name = name;
-        this.age = age;
-        this.cat = cat;
-    }
-
-    public synchronized void printMouseName() {
-        System.out.println(name);
-        cat.printCatAge();
-    }
-
-    public synchronized void printMouseAge() {
-        System.out.println(age);
     }
 }
